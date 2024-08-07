456258ec1164e11d365bfa2f11511d51a69f16f8
kevin5112 <kevin5112@yahoo.com>
2024-08-06 17:34:10 -0700

    update logo to be initials instead of generic

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,10 @@
 export default function HeroSection() {
   return (
     <>
-      <div className="h-lvh place-content-center">
-        <h1 className="">Hero Section</h1>
+      <div className="h-lvh flex items-center justify-center">
+        <h1 className="font-bigShoulder rounded-md border-solid border-2 text-primary border-primary p-5">
+          kc
+        </h1>
         <p className="absolute inset-x-0 bottom-5 text-4xl">v</p>
       </div>
     </>
